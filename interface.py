3d5c873449d1e8ffb7db2afc4ec4f0aecd37b027
Anass <d.anass95100@gmail.com>
2026-05-01 01:40:02 +0200

    Add gitignore

---
+++ interface.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import Label, filedialog
+import main
+
+def bouton_Temp():
+    global cheminTemp
+    cheminTemp = filedialog.askdirectory()
+    label_temp.config(text=cheminTemp)
+
+def bouton_Total():
+    global cheminTot
+    cheminTot = filedialog.askdirectory()
+    label_total.config(text=cheminTot)
+
+def bouton_Resultat():
+    global cheminResultat
+    cheminResultat = filedialog.askdirectory()
+    label_Resultat.config(text=cheminResultat)
+
+def lancerTraitement():
+    if cheminResultat == "" or cheminTemp =="" or cheminTot=="":
+        labelFinal.config(text="Veuiller selectionner tout les dossiers")
+        return
+    pdfCopier = main.traiter_pdf(cheminTot,cheminTemp,cheminResultat)
+    labelFinal.config(text=str(len(pdfCopier))+"PDF copié")
+
+
+fenetre = tk.Tk()
+couleur_fond ="#F4F7FB"
+
+fenetre.title("Filtreur De PDF")
+titre = tk.Label(fenetre, text ="Comparateur intelligent de PDF", font=("Arial",20,"bold"),bg=couleur_fond)
+titre.pack()
+
+fenetre.geometry('900x450')
+fenetre.configure(bg=couleur_fond)
+
+cheminTemp = ""
+cheminTot = ""
+cheminResultat = ""
+
+label_Explication = tk.Label(fenetre, text="Cet outil compare les PDF d’un dossier à analyser avec ceux d’un dossier principal.\n" 
+                             "Les PDF nouveaux sont copiés dans le dossier de sortie et ajoutés au dossier principal.\n"
+                            "Le dossier à analyser est vidé à la fin.",
+                            wraplength=700,
+                            justify="center",font=("Arial", 10), bg=couleur_fond)
+label_Explication.pack(pady=10)
+
+
+cadre_btn = tk.Frame(fenetre,bg=couleur_fond)
+cadre_btn.pack(pady=10, fill="x", padx=40)
+cadre_btn.columnconfigure(0, weight=1)
+cadre_btn.columnconfigure(1, weight=1)
+cadre_btn.columnconfigure(2, weight=1)
+
+
+tk.Button(cadre_btn,text="Dossier à analyser : nouveaux PDF à vérifier", command=bouton_Temp, bg=couleur_fond, cursor="hand2").grid(row=0, column=0, padx=10, sticky="ew")
+label_temp = tk.Label(cadre_btn, text="Aucun dossier choisie",bg=couleur_fond, wraplength=220)
+label_temp.grid(row=1, column=0, pady=8)
+
+tk.Button(cadre_btn, text="Dossier de sortie : PDF uniques trouvés", command=bouton_Resultat, bg=couleur_fond, cursor="hand2").grid(row=0, column=1, padx=10, sticky="ew")
+label_Resultat = tk.Label(cadre_btn,text="Aucun dossier choisie",bg=couleur_fond,wraplength=220)
+label_Resultat.grid(row=1, column=1, pady=8)
+
+tk.Button(cadre_btn, text="Dossier principal : PDF déjà enregistrés", command=bouton_Total, bg=couleur_fond, cursor="hand2").grid(row=0, column=2,padx=10, sticky="ew")
+label_total = tk.Label(cadre_btn,text="Aucun dossier choisie", bg=couleur_fond, wraplength=220)
+label_total.grid(row=1, column=2, pady=8)
+
+labelFinal = tk.Label(fenetre, text="",bg=couleur_fond)
+labelFinal.pack()
+tk.Button(fenetre,text="LANCER", command=lancerTraitement, width=25, bg="#00FF38", font=("Arial",12 ,"bold"),cursor="hand2").pack()
+
+
+
+fenetre.mainloop()
---
+++ main.py
@@ -0,0 +1,101 @@
+import os
+import os.path
+import hashlib
+from shutil import copy2
+
+def filtreOnlyPDF(liste_fichiers):
+    lstFinal =[]
+    for i in liste_fichiers:
+        if i.lower().endswith(".pdf"):
+            lstFinal.append(i)
+    return lstFinal
+
+def hashage(chemin):
+    x = hashlib.sha256()
+    f = open(chemin,"rb")
+    while(True):
+        contains =f.read(4096)
+        if not contains:
+            break
+        x.update(contains)
+    f.close()
+    return x.hexdigest()
+
+
+def PrepareListTotal(lstTot, path):
+    tailleTotal = {}
+    for i in lstTot:
+        chemin = os.path.join(path, i)
+        taille = os.path.getsize(chemin)
+        hashh = hashage(chemin)
+        if taille not in tailleTotal:
+            tailleTotal[taille] = set()
+        tailleTotal[taille].add(hashh)
+    return tailleTotal
+
+
+def trouver_nouveaux_pdf(PDF_temporaire,cheminTemp,lstTotal):
+    ListePDF_a_Copier=[]
+    for i in PDF_temporaire:
+        chemin = os.path.join(cheminTemp,i)
+        sizeTemp = os.path.getsize(chemin)
+        hashh = hashage(chemin)
+        if sizeTemp not in lstTotal:
+            lstTotal[sizeTemp] = set()
+            lstTotal[sizeTemp].add(hashh)
+            ListePDF_a_Copier.append(i)
+        else:
+            if hashh not in lstTotal[sizeTemp] : 
+                lstTotal[sizeTemp].add(hashh)
+                ListePDF_a_Copier.append(i)
+    return ListePDF_a_Copier
+
+
+def copiePDF(ListePDF_a_Copier,cheminTemp,destination_Resultat,destination_total):
+    for i in ListePDF_a_Copier:
+        destination_total_fichier = os.path.join(destination_total, i)
+        destination_resultat_fichier = os.path.join(destination_Resultat, i)
+
+        source = os.path.join(cheminTemp,i)
+
+        copy2(source, destination_total_fichier)
+        copy2(source,destination_resultat_fichier)
+
+def viderDossierTemporaire(lstTempBeforeFiltre,cheminTemp):
+    for i in lstTempBeforeFiltre:
+        path = os.path.join(cheminTemp,i)
+        os.remove(path)
+
+def traiter_pdf(cheminTOT,cheminTemp,cheminResultat):
+    os.makedirs(cheminResultat, exist_ok=True)
+    os.makedirs(cheminTOT, exist_ok=True)
+    os.makedirs(cheminTemp, exist_ok=True)
+
+    lstTempBeforeFiltre = os.listdir(cheminTemp)
+    lstTotBeforeFiltre = os.listdir(cheminTOT)
+
+    PDF_temporaire = filtreOnlyPDF(lstTempBeforeFiltre)
+    lstTot = filtreOnlyPDF(lstTotBeforeFiltre)
+
+    index_total = PrepareListTotal(lstTot,cheminTOT)
+
+    ListePDF_a_Copier = trouver_nouveaux_pdf(PDF_temporaire,cheminTemp,index_total)          
+
+    copiePDF(ListePDF_a_Copier,cheminTemp,cheminResultat,cheminTOT)
+       
+    viderDossierTemporaire(lstTempBeforeFiltre,cheminTemp)
+
+    return ListePDF_a_Copier
+
+
+def main():
+    pdf_copies = traiter_pdf(
+        "dossier_total",
+        "dossier_temporaire",
+        "dossier_resultat"
+    )
+
+    print("PDF copiés :", pdf_copies)
+
+
+main()
